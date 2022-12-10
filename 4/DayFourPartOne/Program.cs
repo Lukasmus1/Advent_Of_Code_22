135c8c9029086a00409d54619b95bc2a367aab5e
Lukáš Píšek <kostici.lopisek@gmail.com>
2022-12-10 18:48:16 +0100

    DayFour part one done

---
+++ 4/DayFourPartOne/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+
+int res = 0;
+foreach (string line in input)
+{
+    string[] lineSplit = line.Split(',');
+    string[] line1;
+    string[] line2;
+
+    line1 = lineSplit[0].Split('-');
+    line2 = lineSplit[1].Split('-');
+
+
+    if (Convert.ToInt32(line1[0]) >= Convert.ToInt32(line2[0]) && Convert.ToInt32(line1[1]) <= Convert.ToInt32(line2[1]))
+    {
+        res++;
+    }
+    else if (Convert.ToInt32(line2[0]) >= Convert.ToInt32(line1[0]) && Convert.ToInt32(line2[1]) <= Convert.ToInt32(line1[1]))
+    {
+        res++;
+    }
+}
+Console.WriteLine(res);
