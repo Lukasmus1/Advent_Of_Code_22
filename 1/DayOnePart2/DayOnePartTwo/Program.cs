c282ca1682c3fc5ee14945e7f9d316c1ec6e3228
Lukáš Píšek <kostici.lopisek@gmail.com>
2022-12-10 15:27:12 +0100

    DayOne done

---
+++ 1/DayOnePart2/DayOnePartTwo/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+
+List<int> listOfElves = new();
+
+int i = 0;
+foreach (string line in input)
+{
+    if (line == "")
+    {
+        listOfElves.Add(i);
+        i = 0;
+        continue;
+    }
+
+    i += int.Parse(line);
+}
+
+int res = 0;
+for (int index = 0; index < 3; index++)
+{
+    int temp = listOfElves.Max();
+    res += temp;
+    listOfElves.Remove(temp);
+}
+
+Console.WriteLine(res);
