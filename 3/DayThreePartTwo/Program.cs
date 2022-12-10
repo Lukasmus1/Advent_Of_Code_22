62ac9ceb1960fc10763f6710aa1bf8a661b60446
Lukáš Píšek <kostici.lopisek@gmail.com>
2022-12-10 18:23:41 +0100

    DayThree part two done

---
+++ 3/DayThreePartTwo/Program.cs
@@ -0,0 +1,29 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+
+List<char> duplicates = new();
+for (int i = 0; i < input.Length; i += 3)
+{
+    foreach (char c in input[i])
+    {
+        if (input[i + 1].Contains(c) && input[i + 2].Contains(c))
+        {
+            duplicates.Add(c);
+            break;
+        }
+    }
+}
+
+int res = 0;
+foreach (char c in duplicates)
+{
+    if (char.IsUpper(c))
+    {
+        res += c - 38;
+    }
+    else
+    {
+        res += c - 96;
+    }
+}
+
+Console.WriteLine(res);
