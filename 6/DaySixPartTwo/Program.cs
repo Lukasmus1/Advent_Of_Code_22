4f1a87b17494b01710993abf920f00f4edc54609
Lukáš Píšek <kostici.lopisek@gmail.com>
2022-12-10 21:50:13 +0100

    6 done

---
+++ 6/DaySixPartTwo/Program.cs
@@ -0,0 +1,18 @@
+﻿string input = File.ReadAllText("input.txt");
+
+char[] arr = new char[14];
+for (int i = 0; i < input.Length; i++)
+{
+    for (int ind = 0; ind < 14; ind++)
+    {
+        arr[ind] = input[ind + i];
+    }
+
+    if (arr.Length == arr.Distinct().Count())
+    {
+        Console.WriteLine(i + 14);
+        break;
+    }
+}
+
+//Weirdly easy day 
