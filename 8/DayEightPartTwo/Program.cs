3e19473bd7d6aebca4e8f19bc325fcd4cdda0191
Lukáš Píšek <kostici.lopisek@gmail.com>
2022-12-11 14:26:26 +0100

    DayEight part one done

--- 8/DayEightPartTwo/Program.cs
+++ 8/DayEightPartTwo/Program.cs
@@ -4,13 +4,13 @@ List<List<int>> input = new();
 int i = 0;
 foreach (string line in inp.Split('\n'))
 {
-	input.Add(new());
-	int ind = 0;
-	foreach (char c in line)
-	{
-		if (c > 47 && c < 58)
-		{
-			input[i].Add(int.Parse(c.ToString()));
+    input.Add(new());
+    int ind = 0;
+    foreach (char c in line)
+    {
+        if (c > 47 && c < 58)
+        {
+            input[i].Add(int.Parse(c.ToString()));
         }
         ind++;
     }
@@ -21,26 +21,28 @@ int res = 0;
 i = 0;
 foreach (var ind in input)
 {
-	for(int i2 = 0; i2 < ind.Count; i2++)
-	{
-		if (i2 == 0 || i2 + 1 == ind.Count || i == 0 || i + 1 == input.Count)
-		{
-			res++;
+    for (int i2 = 0; i2 < ind.Count; i2++)
+    {
+        if (i2 == 0 || i2 + 1 == ind.Count || i == 0 || i + 1 == input.Count)
+        {
+            res++;
+            Console.Write(input[i][i2]);
         }
         else
-		{
-			bool help = true;
+        {
+            bool help = true;
             for (int ind1 = i2 - 1; ind1 >= 0 && help; ind1--)
             {
-				if (input[i][i2] <= input[i][ind1])
-				{
-					help = false;
-					break;
-				}
+                if (input[i][i2] <= input[i][ind1])
+                {
+                    help = false;
+                    break;
+                }
             }
             if (help)
             {
                 res++;
+                Console.Write(input[i][i2]);
                 continue;
             }
 
@@ -49,13 +51,14 @@ foreach (var ind in input)
             {
                 if (input[i][i2] <= input[i][ind1])
                 {
-					help = false;
+                    help = false;
                     break;
                 }
             }
             if (help)
             {
                 res++;
+                Console.Write(input[i][i2]);
                 continue;
             }
 
@@ -71,6 +74,7 @@ foreach (var ind in input)
             if (help)
             {
                 res++;
+                Console.Write(input[i][i2]);
                 continue;
             }
 
@@ -83,15 +87,16 @@ foreach (var ind in input)
                     break;
                 }
             }
-            if(help)
+            if (help)
             {
                 res++;
+                Console.Write(input[i][i2]);
                 continue;
             }
         }
     }
-	i++;
+    Console.WriteLine();
+    i++;
 }
-Console.WriteLine(res);
 
-//Ugly but working 
+Console.WriteLine(res);
