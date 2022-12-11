4e9ee58a2f98ac52117a75e350cdd94d5f8b5487
Lukáš Píšek <kostici.lopisek@gmail.com>
2022-12-11 01:38:37 +0100

    Part one 2+ hours trollDespair

---
+++ 7/DaySevenPartOne/Program.cs
@@ -0,0 +1,65 @@
+﻿List<string> currentPath = new();
+Dictionary<string, int> dirs = new()
+{
+    { "root", 0 }
+};
+
+string[] input = File.ReadAllLines("input.txt");
+
+int totalSize = 0;
+foreach (string line in input)
+{
+    if (line[0] == '$')
+    {
+        string temp = line.Remove(0, 2);
+        if (temp.Remove(2) == "cd")
+        {
+            switch (temp.Remove(0, 3))
+            {
+                case "..":
+                    currentPath.RemoveAt(currentPath.Count - 1);
+                    break;
+
+                case "/":
+                    currentPath.Clear();
+                    currentPath.Add("root");
+                    break;
+
+                default:
+                    currentPath.Add(line.Remove(0, 5));
+                    string s = "";
+                    foreach (string str in currentPath)
+                    {
+                        s += str;
+                    }
+                    if (!dirs.ContainsKey(s))
+                    {
+                        dirs.Add(s, 0);
+                    }
+                    break;
+            }
+        }
+        continue;
+    }
+
+    if (line.Remove(3) != "dir")
+    {
+        string[] file = line.Split(' ');
+        string s = "";
+        foreach (string str in currentPath)
+        {
+            s += str;
+            dirs[s] += int.Parse(file[0]);
+        }
+    }
+}
+
+foreach (var dic in dirs)
+{
+    if (dic.Value <= 100000)
+    {
+        totalSize += dic.Value;
+    }
+}
+
+Console.WriteLine(totalSize);
