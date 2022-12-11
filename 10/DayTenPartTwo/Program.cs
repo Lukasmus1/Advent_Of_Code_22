b4514d94052735157f2a334660251bdd6f2368be
Lukáš Píšek <kostici.lopisek@gmail.com>
2022-12-11 22:58:53 +0100

    Day 10 part two done

---
+++ 10/DayTenPartTwo/Program.cs
@@ -0,0 +1,43 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+
+int cycleCount = -1;
+int register = 1;
+foreach (string line in input)
+{
+    cycleCount++;
+
+    if (cycleCount == 40)
+    {
+        cycleCount = 0;
+        Console.WriteLine();
+    }
+
+    if (cycleCount >= register - 1 && cycleCount <= register + 1)
+    {
+        Console.Write("#");
+    }
+    else
+    {
+        Console.Write(".");
+    }
+
+    if(line.Remove(4) == "addx")
+    {
+        cycleCount++;
+        if (cycleCount == 40)
+        {
+            cycleCount = 0;
+            Console.WriteLine();
+        }
+
+        if (cycleCount >= register - 1 && cycleCount <= register + 1)
+        {
+            Console.Write("#");
+        }
+        else
+        {
+            Console.Write(".");
+        }
+        register += int.Parse(line.Remove(0, 5));
+    }
+}
